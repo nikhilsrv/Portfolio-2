c608b8f130e35f3e9f15421eafadadc3a71eb8bf
Nikhil Srivastava <nikhilsrv2017@gmail.com>
2026-01-19 00:15:10 +0530

    Add lottie-react and swiper dependencies; implement scrollbar styling and project component

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
-import AboutLayout from "./components/AboutComponents/AboutLayout";
-import HeroSection from "./components/HomeComponents/HeroSection";
-import Experience from "./components/ExperienceComponents";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-
+import AboutLayout from './components/AboutComponents/AboutLayout';
+import HeroSection from './components/HomeComponents/HeroSection';
+import Experience from './components/ExperienceComponents';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import '@/app/App.css';
+import Projects from './components/Project';
 
 export default function Home() {
   return (
     <>
-      <HeroSection/>
-      <AboutLayout/>
-      <Experience/>
-      <Contact/>
-      <Footer/>
+      <HeroSection />
+      <AboutLayout />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
